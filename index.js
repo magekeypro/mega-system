fc13e43baac5fea363109d8780a1390d2274f2d0
magekeypro <galaxymaneg@gmail.com>
2026-03-29 23:00:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const app = express();
 app.use(express.json());
 
 // 🔗 حط هنا الرابط بتاعك
-const uri = "mongodb+srv://galaxymaneg_db_user:<5tLXk3DR8VHrH2RL
->@mega.s42tt5h.mongodb.net/?appName=mega";
-
+const uri = "mongodb+srv://galaxymaneg_db_user:5tLXk3DR8VHrH2RL@mega.s42tt5h.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 let db;
