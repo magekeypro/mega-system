504ee0a314ca5ca6a37ba9df2c75c14021a217ef
magekeypro <galaxymaneg@gmail.com>
2026-03-29 22:05:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+// test route
 app.get('/', (req, res) => {
-  res.send('MEGA SYSTEM WORKING 🚀');
+  res.send('MEGA API WORKING 🚀');
+});
+
+// get clients
+app.get('/clients', (req, res) => {
+  res.json([
+    { name: "Ahmed", phone: "0100000000", status: "active" }
+  ]);
+});
+
+// add client
+app.post('/clients', (req, res) => {
+  const data = req.body;
+  res.json({
+    message: "Client added",
+    data: data
+  });
 });
 
 app.listen(3000, () => {
